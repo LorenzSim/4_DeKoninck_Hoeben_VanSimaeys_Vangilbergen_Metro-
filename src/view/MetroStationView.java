86791b77c3d42713be06ca2d1f35fbe069485cd8
Lorenz Van Simaeys <lorenz.vansimaeys@student.ucll.be>
2022-12-14 16:13:47 +0100

    added metrocard id selectors for testing purposes

--- src/view/MetroStationView.java
+++ src/view/MetroStationView.java
@@ -1,8 +1,10 @@
 package view;
 
 import controller.MetroStationViewController;
+import javafx.collections.FXCollections;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.MetroFacade;
@@ -12,14 +14,20 @@ import java.util.List;
 public class MetroStationView {
 	
 	private final Stage stage = new Stage();
-	
+	ChoiceBox<Integer> idSelector;
+
+
 	public MetroStationView(MetroStationViewController controller){
+		idSelector = new ChoiceBox<>();
+
 		controller.setView(this);
 		stage.setTitle("METRO STATION VIEW");
 		stage.initStyle(StageStyle.UTILITY);
 		stage.setX(5);
 		stage.setY(390);
 		Group root = new Group();
+		root.getChildren().add(idSelector);
+
 		Scene scene = new Scene(root, 650, 300);			
 		stage.setScene(scene);
 		stage.sizeToScene();			
@@ -27,6 +35,6 @@ public class MetroStationView {
 	}
 
 	public void updateMetroCardIDList(List<Integer> IDs){
-
+		idSelector.setItems(FXCollections.observableArrayList(IDs));
 	}
 }
--- src/view/MetroTicketView.java
+++ src/view/MetroTicketView.java
@@ -2,12 +2,15 @@ package view;
 
 import controller.MetroTicketViewController;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,9 +19,12 @@ import java.util.List;
 
 
 public class MetroTicketView {
-	private Stage stage = new Stage();
+	private final Stage stage = new Stage();
+	ChoiceBox<Integer> idSelector;
 
 	public MetroTicketView(MetroTicketViewController controller){
+		idSelector = new ChoiceBox<>();
+
 		controller.setView(this);
 		stage.setTitle("METROTICKET VIEW");
 		stage.initStyle(StageStyle.UTILITY);
@@ -31,7 +37,10 @@ public class MetroTicketView {
 			controller.buyMetroCard();
 		};
 		newMetroCardButton.setOnAction(event);
+
 		newMetroCardTilePane.getChildren().add(newMetroCardButton);
+		newMetroCardTilePane.getChildren().add(idSelector);
+
 		root.getChildren().add(newMetroCardTilePane);
 		Scene scene = new Scene(root, 650, 350);
 		stage.setScene(scene);
@@ -40,7 +49,7 @@ public class MetroTicketView {
 	}
 
 	public void updateMetroCardIDList(List<Integer> IDs){
-
+		idSelector.setItems(FXCollections.observableArrayList(IDs));
 	}
 
 }
