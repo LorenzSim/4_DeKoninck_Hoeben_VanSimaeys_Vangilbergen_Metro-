a9121ae18a5d356a191d56ac55d97bbce36235d3
Lorenz Van Simaeys <lorenz.vansimaeys@student.ucll.be>
2022-12-29 19:16:50 +0100

    improved setupPane ui

--- src/view/panels/SetupPane.java
+++ src/view/panels/SetupPane.java
@@ -3,9 +3,11 @@ package view.panels;
 
 import controller.SetupPaneController;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import model.TicketPriceDecorator.TicketPriceDiscountEnum;
@@ -19,25 +21,25 @@ import java.util.List;
 
 public class SetupPane extends GridPane{
 
-    private VBox discountVbox = new VBox();
+    private VBox discounts = new VBox();
 
 
     public SetupPane(SetupPaneController controller){
         controller.setView(this);
+        discounts.setSpacing(10);
+
+        HBox container = new HBox();
+        container.setPadding(new Insets(10,0,0,10));
+        container.setSpacing(50);
+
+        VBox saveFileTypeBox = new VBox();
+        saveFileTypeBox.setSpacing(10);
 
         Text selectLSS = new Text("Metrocard file type: ");
-        selectLSS.setTranslateX(20);
-
         ChoiceBox<String> lSSOptions = new ChoiceBox<>();
-        lSSOptions.setTranslateX(20);
-        lSSOptions.setTranslateY(50);
-
         initLoadSaveStrategyDropDown(lSSOptions);
 
         Button saveButton = new Button("Save file type");
-        saveButton.setTranslateX(20);
-        saveButton.setTranslateY(100);
-
         EventHandler<ActionEvent> event = e -> {
             controller.saveLoadSaveStrategy(LoadSaveStrategyEnum.valueOf(lSSOptions.getValue()));
             Text saved = new Text("Settings are saved!");
@@ -46,31 +48,29 @@ public class SetupPane extends GridPane{
             getChildren().add(saved);
         };
         saveButton.setOnAction(event);
+        saveFileTypeBox.getChildren().addAll(selectLSS, lSSOptions, saveButton);
 
+
+        VBox discountBox = new VBox();
+        discountBox.setSpacing(10);
         for (TicketPriceDiscountEnum discount : TicketPriceDiscountEnum.values()) {
             CheckBox checkBox = new CheckBox(discount.name());
-            discountVbox.getChildren().add(checkBox);
+            discounts.getChildren().add(checkBox);
         }
-
-        discountVbox.setTranslateX(20);
-        discountVbox.setTranslateY(220);
-
         Button discountButton = new Button("Save discount");
-        discountButton.setTranslateX(20);
-        discountButton.setTranslateY(discountVbox.getTranslateY() + 50);
-
         EventHandler<ActionEvent> saveDiscounts = e -> {
-            ArrayList<String> discounts = new ArrayList<>(discountVbox.getChildren().size());
-            for (Node d : discountVbox.getChildren()) {
+            ArrayList<String> discounts = new ArrayList<>(this.discounts.getChildren().size());
+            for (Node d : this.discounts.getChildren()) {
                 if (((CheckBox) d).isSelected()) {
                     discounts.add(((CheckBox) d).getText());
                 }
             }
             controller.savePriceDiscounts(discounts);
         };
-
         discountButton.setOnAction(saveDiscounts);
-        getChildren().addAll(selectLSS, lSSOptions, saveButton, discountVbox, discountButton);
+        discountBox.getChildren().addAll(discounts, discountButton);
+        container.getChildren().addAll(saveFileTypeBox, discountBox);
+        getChildren().addAll(container);
 
     }
 
@@ -84,7 +84,7 @@ public class SetupPane extends GridPane{
     }
 
     public void initDiscounts(List<String> discounts){
-        for (Node discount : discountVbox.getChildren()) {
+        for (Node discount : this.discounts.getChildren()) {
             if (discounts.contains(((CheckBox) discount).getText())) {
                  ((CheckBox) discount).setSelected(true);
             }
